1eb91665abbf4d54826beed13b98e4fa52d58538
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-07 21:47:58 +0200

    Avancement

--- src/test/java/org/example/Order.java
+++ src/test/java/org/example/Order.java
@@ -1,15 +1,12 @@
 package org.example;
 
 public class Order {
-    private String message;
-    private Drink drinkType;
-    private int sugarNumber;
-    private Boolean stick = false;
+    private final Drink drinkType;
+    private final int sugarNumber;
 
     public Order(Drink drinkType, int sugarNumber) {
         this.drinkType = drinkType;
         this.sugarNumber = sugarNumber;
-        if(sugarNumber > 0) this.stick = true;
     }
 
     public String showMessage(){
