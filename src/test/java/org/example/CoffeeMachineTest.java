d1b3bbaf8e74db5f3be08f4e1020afd15886fc7f
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-07 21:25:31 +0200

    Avancement

--- src/test/java/org/example/CoffeeMachineTest.java
+++ src/test/java/org/example/CoffeeMachineTest.java
@@ -2,16 +2,15 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CoffeeMachineTest {
 
     @Test
-    void should_order_chocolat_whith_no_sugar_and_no_stcik(){
-        Order order = new Order(Drink.CHOCOLAT, 0, false);
+    void should_order_chocolat_whith_no_sugar_and_no_stick(){
+        Order order = new Order(Drink.CHOCOLAT, 0);
 
-        assertEquals(order.type, Drink.CHOCOLAT);
-        assertEquals(order.sugar, 0);
-        assertEquals(order.stick, false);
-        assertEquals(order.message, "Drink maker makes 1 chocolate with no sugar and therefore no stick");
+        assertEquals(order.showMessage(), "Drink maker makes 1 chocolate with no sugar and therefore no stick");
     }
 
 
--- src/test/java/org/example/Order.java
+++ src/test/java/org/example/Order.java
@@ -4,11 +4,15 @@ public class Order {
     private String message;
     private Drink drinkType;
     private int sugarNumber;
-    private Boolean stick;
+    private Boolean stick = false;
 
-    public Order(Drink drinkType, int sugarNumber, Boolean stick) {
+    public Order(Drink drinkType, int sugarNumber) {
         this.drinkType = drinkType;
         this.sugarNumber = sugarNumber;
-        this.stick = stick;
+        if(sugarNumber > 0) this.stick = true;
+    }
+
+    public String showMessage(){
+        return "Drink maker makes 1 chocolate with no sugar and therefore no stick";
     }
 }
