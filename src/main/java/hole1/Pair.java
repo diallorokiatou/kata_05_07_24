f3eb6165a21ec53ab9d4b48d354b4504742be6ab
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-08 09:33:36 +0200

    Avancement

--- src/main/java/hole1/Pair.java
+++ src/main/java/hole1/Pair.java
@@ -1,12 +1,5 @@
 package hole1;
 
-class Pair<A, B> {
-    final A first;
-    final B second;
-
-    Pair(A first, B second) {
-        this.first = first;
-        this.second = second;
-    }
+record Pair<A, B>(A first, B second) {
 
 }
