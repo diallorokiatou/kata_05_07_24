9c5e421499f10afc3988fc4e04cdaabe544dbc2d
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-08 16:14:11 +0200

    avancement

--- src/main/java/hole1/TaxRate.java
+++ src/main/java/hole1/TaxRate.java
@@ -4,7 +4,7 @@ import static hole1.Money.money;
 
 public record TaxRate(int percent) {
     Money apply(Money total) {
-        Double amount = total.value * (percent() / 100d);
+        Double amount = total.value * (percent / 100d);
         return money(amount.intValue(), total.currency);
     }
 }
