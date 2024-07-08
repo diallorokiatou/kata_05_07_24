56a151348366053da88cde89a2d5a507be313558
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-08 08:44:29 +0200

    Avancement

--- src/main/java/org/example/GossipingBusDrivers.java
+++ src/main/java/org/example/GossipingBusDrivers.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import java.util.List;
+
 public class GossipingBusDrivers {
 
+    private final List<Integer> routes;
+
+    public GossipingBusDrivers(List<Integer> routes) {
+
+        this.routes = routes;
+    }
+
+    public String numberOfGossipingStop() {
+        return "never";
+    }
 }
