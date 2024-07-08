8d14c8f437875ae1c690916810f1a6b89de09b68
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-08 08:43:32 +0200

    Avancement

---
+++ src/test/java/org/example/GossipingBusDriversTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GossipingBusDriversTest {
+
+    @Test
+    void no_gossip(){
+        List<Integer> routes = new ArrayList();
+        GossipingBusDrivers gossipingBusDrivers = new GossipingBusDrivers(routes);
+
+        String numberOfGossipingStop = gossipingBusDrivers.numberOfGossipingStop();
+
+        assertEquals("never",numberOfGossipingStop);
+    }
+
+
+}
