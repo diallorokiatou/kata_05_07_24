44ade933802981a9c74e2e48879792ae7d7b9f48
diallorokiatou <40149545+diallorokiatou@users.noreply.github.com>
2024-07-08 16:40:04 +0200

    avancement

--- src/main/java/hole1/TakeHomeCalculator.java
+++ src/main/java/hole1/TakeHomeCalculator.java
@@ -11,7 +11,6 @@ class TakeHomeCalculator {
     }
 
     Money netAmount(Money first, Money... rest) {
-
         Money total = Arrays.stream(rest).reduce(first, Money::plus);
         Money tax = taxRate.apply(total);
         return total.minus(tax);
